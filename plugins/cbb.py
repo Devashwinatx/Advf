2371624c714d3e18a47fb514469759ae35ae528d
Devashwinatx <ashwinrajaashwin7@gmail.com>
2025-01-13 19:31:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╔══════════════════════════════⦿\n➥ ᴄʀᴇᴀᴛᴏʀ :<a href='tg://user?id={8108281129}'>『𝚈𝚊𝚎 𝙼𝚒𝚔𝚘』❋𝄗⃝🦋 ⌞𝚆𝚊𝚛𝚕𝚘𝚛𝚍𝚜⌝ ㊋ </a>\n╚══════════════════════════════⦿</b>",disable_web_page_preview = True,
+            text = f"<b>╔══════════════════════════════⦿\n➥ Developer:<a href='tg://user?id={6121610691}'>NARUTO ㊋ </a>\n╚══════════════════════════════⦿</b>",disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
